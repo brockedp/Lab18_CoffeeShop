82e7fff48b87d9a5139f0ee930b1399060a0f27c
Deno <denobrockert@yahoo.com>
2019-08-18 17:41:13 -0400

    First push of lab 18

---
+++ Lab18_CoffeeShop/Controllers/RegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab18_CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18_CoffeeShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new RegisterUser());
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(RegisterUser user)
+        {
+            return RedirectToAction("Summary", user);
+        }
+        
+        public IActionResult Summary(RegisterUser user)
+        {
+            return View(user);
+        }
+
+
+        
+    }
+}
